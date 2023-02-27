0899b8fe7486b8953cabc3396547ad44704bf420
nemke91 <126467705+nemke91@users.noreply.github.com>
2023-02-27 12:01:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	
+	var videoContainer = document.querySelector(".video-container");
+  
+  setTimeout(function() {
+    videoContainer.style.opacity = "1";
+  
+  }, 500);
+
+};
